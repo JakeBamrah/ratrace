f4ed05b25c6db6d5ad2acf80ed9650646829ba29
Jake Bamrah <login@bamrah.sh>
2023-03-16 14:45:52 +0000

    update build config and add build visualizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
     port: 3000,
     host: true
   },
-  plugins: [svelte()],
+  build: {
+      outDir: 'build'
+  },
+  plugins: [svelte(), visualizer()],
 })
